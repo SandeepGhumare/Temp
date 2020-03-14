84da3b6f74b838b08d12ea88f03310932feb07e3
Sandeep <sandeep.ghumare@hotmail.com>
2020-03-14 13:35:56 +0530

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { StudentComponent } from './student/student.component';
+import { Sample1Component } from './sample1/sample1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmployeeComponent,
-    StudentComponent
+    StudentComponent,
+    Sample1Component
   ],
   imports: [
     BrowserModule
